efffe8ff052b62c1a2328f96f4dd6cc68837283a
harryng <little.ruan@gmail.com>
2021-09-06 11:50:53 +0700

    layout + data input form dialog Navigation, Animation, App State Localization with 02 methods l10n, i18n Form: validate and save form data Prepare for advanced

--- lib/sqlite/test_sqlite.dart
+++ lib/sqlite/test_sqlite.dart
@@ -37,35 +37,35 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<Dog> lsDataDog = [];
   List<Dog> lsSearchDog = List.empty(growable: true);
+  GlobalKey pnlResultKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController txtSearchCtr = TextEditingController(text: "");
-    // txtSearchCtr.addListener(() {
-    //   final text = txtSearchCtr.text.toLowerCase();
-    //   txtSearchCtr.value = txtSearchCtr.value.copyWith(
-    //     text: text,
-    //     selection: TextSelection(baseOffset: text.length, extentOffset: text.length),
-    //     composing: TextRange.empty,
-    //   );
-    // });
+    TextEditingController txtSearchCtr = TextEditingController();
     ValueChanged<String>? searchDog = (String text) {
-      setState(() {
-        // txtSearchCtr.text=text;
-        List<Dog> rs = List.empty(growable: false);
-        if (text.isNotEmpty) {
-          rs = lsDataDog
-              .where((element) =>
-                  element.name.toLowerCase().contains(text.toLowerCase()))
-              .toList();
-        }
-        lsSearchDog = rs;
+      // setState(() {
+      List<Dog> rs = List.empty(growable: false);
+      if (text.isNotEmpty) {
+        rs = lsDataDog
+            .where((element) =>
+                element.name.toLowerCase().contains(text.toLowerCase()))
+            .toList();
+      }
+      lsSearchDog.clear();
+      lsSearchDog.addAll(rs);
+      // });
+    };
+    VoidCallback? refreshData = () {
+      pnlResultKey.currentState!.setState(() {
+        txtSearchCtr.clear();
+        lsSearchDog.clear();
+        lsSearchDog.addAll(lsDataDog);
       });
     };
     return Scaffold(
         body: CustomScrollView(slivers: [
       PreferredSize(
-          preferredSize: Size.fromHeight(140.0),
+          preferredSize: Size.fromHeight(140.0), // not affected
           child: SliverAppBar(
             title: Text('Dog list'),
             floating: true,
@@ -78,26 +78,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
                   child: Container(
                     height: 30.0,
-                    child: TextField(
-                      controller: txtSearchCtr,
-                      keyboardType: TextInputType.text,
-                      textInputAction: TextInputAction.search,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.search),
-                          suffixIcon: IconButton(
-                            iconSize: 20,
-                            icon: Icon(Icons.clear),
-                            onPressed: () {
-                              txtSearchCtr.clear();
-                            },
-                          ),
-                          // border: InputBorder.none,
-                          // focusedBorder: InputBorder.none,
-                          contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
-                          hintText: "Search"),
-                      // onChanged: searchDog,
-                      onSubmitted: searchDog,
-                      // onSubmitted: searchDog,
+                    child: GestureDetector(
+                      child: TextField(
+                        controller: txtSearchCtr,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.search,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            suffixIcon: IconButton(
+                              iconSize: 20,
+                              icon: Icon(Icons.clear),
+                              onPressed: refreshData,
+                            ),
+                            // border: InputBorder.none,
+                            // focusedBorder: InputBorder.none,
+                            contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                            hintText: "Search"),
+                        onChanged: (text) {
+                          pnlResultKey.currentState!.setState(() {
+                            searchDog(text);
+                          });
+                        },
+                        onSubmitted: (text) {
+                          pnlResultKey.currentState!.setState(() {
+                            searchDog(text);
+                          });
+                        },
+                      ),
                     ),
                   ),
                 ),
@@ -105,12 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             leading: IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: () {
-                setState(() {
-                  txtSearchCtr.clear();
-                  lsSearchDog = lsDataDog;
-                });
-              },
+              onPressed: refreshData,
             ),
             actions: [
               IconButton(
@@ -124,12 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       int age = rand.nextInt(100);
                       Dog dog = Dog(id: id, name: name, age: age);
                       lsDataDog.add(dog);
-                      // clear the search
-                      txtSearchCtr.clear();
-                      // assign the result
-                      // lsSearchDog.clear();
-                      // lsSearchDog.addAll(lsDataDog);
-                      lsSearchDog = lsDataDog;
+                      refreshData();
                     });
                   }),
               IconButton(
@@ -139,66 +136,77 @@ class _MyHomePageState extends State<MyHomePage> {
                       if (lsDataDog.length > 0) {
                         txtSearchCtr.clear();
                         lsDataDog.removeLast();
-                        // lsSearchDog.clear();
-                        // lsSearchDog.addAll(lsDataDog);
-                        lsSearchDog = lsDataDog;
+                        refreshData();
                       }
                     });
                   }),
             ],
           )),
-      if (lsSearchDog.length > 0)
-        SliverPadding(
-          padding: EdgeInsets.zero,
-          sliver: SliverList(
-            // Use a delegate to build items as they're scrolled on screen.
-            delegate: SliverChildBuilderDelegate(
-              // The builder function returns a ListTile with a title that
-              // displays the index of the current item.
-              (context, index) {
-                return Card(
-                  shadowColor: Colors.transparent,
-                  margin: EdgeInsets.only(left: 1, top: 1, right: 1, bottom: 0),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(width: 1, color: Colors.cyan),
-                    borderRadius: BorderRadius.circular(15.0),
+      StatefulBuilder(
+          key: pnlResultKey,
+          builder: (context, setState) {
+            Widget widget;
+            if (lsSearchDog.length > 0) {
+              widget = SliverPadding(
+                padding: EdgeInsets.zero,
+                sliver: SliverList(
+                  // Use a delegate to build items as they're scrolled on screen.
+                  delegate: SliverChildBuilderDelegate(
+                    // The builder function returns a ListTile with a title that
+                    // displays the index of the current item.
+                    (context, index) {
+                      return Card(
+                        shadowColor: Colors.transparent,
+                        margin: EdgeInsets.only(
+                            left: 1, top: 1, right: 1, bottom: 0),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1, color: Colors.cyan),
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Container(
+                          // decoration: BoxDecoration(
+                          //   shape: BoxShape.rectangle,
+                          //   border: Border.all(color: Colors.cyan)
+                          // ),
+                          padding: EdgeInsets.all(5),
+                          margin: EdgeInsets.zero,
+                          child: Column(children: [
+                            Row(children: [
+                              Expanded(
+                                  child: Text('ID:${lsSearchDog[index].id}'))
+                            ]),
+                            Row(children: [
+                              Expanded(
+                                  child:
+                                      Text('Name:${lsSearchDog[index].name}'))
+                            ]),
+                            Row(children: [
+                              Expanded(
+                                  child: Text('Age:${lsSearchDog[index].age}'))
+                            ]),
+                          ]),
+                        ),
+                      );
+                    },
+                    // Builds 1000 ListTiles
+                    childCount: lsSearchDog.length,
                   ),
-                  child: Container(
-                    // decoration: BoxDecoration(
-                    //   shape: BoxShape.rectangle,
-                    //   border: Border.all(color: Colors.cyan)
-                    // ),
-                    padding: EdgeInsets.all(5),
-                    margin: EdgeInsets.zero,
-                    child: Column(children: [
-                      Row(children: [
-                        Expanded(child: Text('ID:${lsSearchDog[index].id}'))
-                      ]),
-                      Row(children: [
-                        Expanded(child: Text('Name:${lsSearchDog[index].name}'))
-                      ]),
-                      Row(children: [
-                        Expanded(child: Text('Age:${lsSearchDog[index].age}'))
-                      ]),
-                    ]),
+                ),
+              );
+            } else {
+              widget = SliverToBoxAdapter(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Center(child: Text('Record is not found')))
+                    ],
                   ),
-                );
-              },
-              // Builds 1000 ListTiles
-              childCount: lsSearchDog.length,
-            ),
-          ),
-        )
-      else
-        SliverToBoxAdapter(
-          child: Container(
-            child: Row(
-              children: [
-                Expanded(child: Center(child: Text('Record is not found')))
-              ],
-            ),
-          ),
-        ),
+                ),
+              );
+            }
+            return widget;
+          }),
     ]));
   }
 }
